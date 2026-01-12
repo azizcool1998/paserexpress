18b282396239682bf790e9fb4429a95d5e32a968
azizcool1998 <azizcool1998@gmail.com>
2026-01-13 01:16:51 +0800

    Update welcome.php

--- src/pages/welcome.php
+++ src/pages/welcome.php
@@ -1,1 +1,11 @@
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Selamat datang di Website Kami!</h1>
+    <p>Ini adalah halaman utama. Silakan jelajahi konten kami.</p>
+</body>
+</html>
