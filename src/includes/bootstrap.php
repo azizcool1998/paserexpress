763fe8e6e161a0cc27f8834210706115a0ddc9ae
azizcool1998 <azizcool1998@gmail.com>
2026-01-13 01:35:28 +0800

    Create bootstrap.php

---
+++ src/includes/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/env.php';
+require_once __DIR__ . '/helpers.php';
+require_once __DIR__ . '/db.php';
+require_once __DIR__ . '/csrf.php';
+require_once __DIR__ . '/auth.php';
+
+date_default_timezone_set('Asia/Makassar');
