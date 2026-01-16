a2b03356e3ae7a370243406c9d8ed3ee257a4d33
azizcool1998 <azizcool1998@gmail.com>
2026-01-16 17:26:00 +0800

    Update AdminController.php

--- src/controllers/AdminController.php
+++ src/controllers/AdminController.php
@@ -14,4 +14,25 @@ class AdminController
 
         view("admin_dashboard", compact('users'));
     }
+
+    public function backupSettings() {
+        $settings = load_backup_settings();
+        render("admin/admin_backup", compact("settings"));
+    }
+
+    public function backupSave() {
+        $enabled = $_POST['enabled'] ?? 'off';
+        $interval = $_POST['interval'] ?? '1d';
+
+        save_backup_settings($enabled, $interval);
+        generate_cron_backup($enabled, $interval);
+
+        redirect("?page=admin_backup&saved=1");
+    }
+
+    public function backupRunNow() {
+        require_once __DIR__ . "/../services/backup_service.php";
+        run_backup_now();
+        redirect("?page=admin_backup&run=1");
+    }
 }
