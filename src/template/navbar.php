e34e69c7cbdd6fa7dd0447d9bed10617575f3b7d
azizcool1998 <azizcool1998@gmail.com>
2026-01-16 18:55:06 +0800

    Create navbar.php

---
+++ src/template/navbar.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg god-topbar shadow-sm px-3">
+    <div class="container-fluid">
+
+        <button class="btn btn-outline-secondary me-3" id="sidebarToggle">
+            <i class="bi bi-list fs-5"></i>
+        </button>
+
+        <span class="navbar-brand fw-bold">Admin Panel</span>
+
+        <div class="ms-auto">
+
+            <button id="themeToggle" class="btn btn-outline-primary">
+                <i class="bi bi-sun-fill theme-icon-active"></i>
+                <i class="bi bi-moon-stars-fill theme-icon-dark"></i>
+            </button>
+
+        </div>
+    </div>
+</nav>
