7f974cac4cd806fa5c7f1b3196cb8b2cfc5bbbc5
azizcool1998 <azizcool1998@gmail.com>
2026-01-16 18:34:26 +0800

    Create layout_empty.php

---
+++ src/template/layout_empty.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="auto">
+<head>
+    <meta charset="UTF-8">
+    <title><?= htmlspecialchars($title) ?></title>
+
+    <!-- Bootstrap 5.3 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Icons -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-dark text-white">
+
+<?= $content ?>
+
+</body>
+</html>
